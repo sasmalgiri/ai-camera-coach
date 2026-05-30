c10c30e005d3ce8a0813b47a54f20e1e9d31cdd9
sasmalgiri <sasmalgiri@gmail.com>
2026-05-31 03:30:26 +0530

    Initial Commit

---
+++ original photo and ai assistant/original_photo_and_ai_assistantApp.swift
@@ -0,0 +1,17 @@
+//
+//  original_photo_and_ai_assistantApp.swift
+//  original photo and ai assistant
+//
+//  Created by Shirshendu Sasmal on 31/05/26.
+//
+
+import SwiftUI
+
+@main
+struct original_photo_and_ai_assistantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
